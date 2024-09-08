09d782a3f7e16588f99073ee82cc9b76e9423476
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2024-09-08 12:01:10 +0200

    Initial commit

---
+++ HullMaker/Assets/Scripts/Data/MainData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Data {
+    public class MainData : MonoBehaviour {
+
+        public int mass = 1000;
+        public Mesh mesh;
+        public int numOfMat = 1;
+        public Material[] materials;
+        public int numOfCol = 1;
+        public Mesh[] colliders;
+
+    }
+} //END
---
+++ HullMaker/Assets/Scripts/Utils/Tags.cs
@@ -0,0 +1,11 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Utils {
+    public class Tags : MonoBehaviour {
+
+        public const string HULL_VISUAL = "HullVisual";
+        public const string PLATES = "Plates";
+
+    }
+}//END
