38864de26b0d03f8337c3dd0e8d1403cb3a9ab42
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2024-09-08 13:28:23 +0200

    Created package for hull

---
+++ InitialTemplate/Assets/Scripts/TemplateImporter.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using UnityEditor;
+using UnityEngine;
+
+public class TemplateImporter : EditorWindow {
+
+    [MenuItem("Tools/Template Importer")]
+    public static void ShowWindow() {
+        GetWindow<TemplateImporter>("Template Importer");
+    }
+
+    private void OnGUI() {
+        GUILayout.Label("Import Project Template", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Import HULL template")) {
+            ImportTemplate("https://github.com/yourrepo/Template1.unitypackage");
+        }
+
+        // Add more buttons for other templates
+    }
+
+    private void ImportTemplate(string url) {
+        string packagePath = Application.dataPath + "/DownloadedTemplate.unitypackage";
+
+        using (WebClient client = new WebClient()) {
+            client.DownloadFile(url, packagePath);
+        }
+
+        AssetDatabase.ImportPackage(packagePath, true);
+
+        // Add your custom setup code here (e.g., configuring scenes, assets, etc.)
+    }
+
+} //END
